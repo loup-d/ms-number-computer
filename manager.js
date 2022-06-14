a890d971cce7e3ff9eecb02d6da26277e7ea85d4
Loup Dallier <loup.dallier@epita.fr>
2022-06-14 14:07:07 +0200

    add sentry to code

--- manager.js
+++ manager.js
@@ -1,6 +1,38 @@
 const MM = require("ms-manager");
 let config = require(`./config.json`) || {};
 
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+    dsn: "https://932dc28c8a2e48b8a07d611b574dd9d0@o1287347.ingest.sentry.io/6504598",
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+    op: "test",
+    name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+    try {
+        foo();
+    } catch (e) {
+        Sentry.captureException(e);
+    } finally {
+        transaction.finish();
+    }
+}, 99);
+
 MM.init(config, (err, serviceInfo) => {
     if (err) {
         return console.error(err);
